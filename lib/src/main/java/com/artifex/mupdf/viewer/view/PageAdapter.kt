3bae6ea70e781ae3db6083c06446cddc5b430e44
Khalid <khalid.benni@gmail.com>
2023-10-04 16:04:35 +0200

    Migration of PageAdapter AsyncTask to Coroutine

--- lib/src/main/java/com/artifex/mupdf/viewer/view/PageAdapter.kt
+++ lib/src/main/java/com/artifex/mupdf/viewer/view/PageAdapter.kt
@@ -4,12 +4,15 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Point
 import android.graphics.PointF
-import android.os.AsyncTask
 import android.util.SparseArray
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import com.artifex.mupdf.viewer.core.MuPDFCore
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class PageAdapter(
     private val context: Context,
@@ -36,7 +39,6 @@ class PageAdapter(
     }
 
     fun releaseBitmaps() {
-        //  recycle and release the shared bitmap.
         bitmap?.recycle()
         bitmap = null
     }
@@ -47,60 +49,61 @@ class PageAdapter(
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
-        val pageView: PageView
+        val pageView: PageView = if (convertView == null) {
 
-        if (convertView == null) {
             if (bitmap == null || bitmap!!.width != parent.width || bitmap!!.height != parent.height) {
-                bitmap = if (parent.width > 0 && parent.height > 0) Bitmap.createBitmap(
-                    parent.width,
-                    parent.height,
-                    Bitmap.Config.ARGB_8888
-                ) else null
+
+                bitmap = if (parent.width > 0 && parent.height > 0) {
+                    Bitmap.createBitmap(
+                        /* width = */ parent.width,
+                        /* height = */ parent.height,
+                        /* config = */ Bitmap.Config.ARGB_8888
+                    )
+                } else {
+                    null
+                }
+
             }
-            pageView = PageView(context, muPDFCore, Point(parent.width, parent.height), bitmap)
+
+            PageView(context, muPDFCore, Point(parent.width, parent.height), bitmap)
+
         } else {
-            pageView = convertView as PageView
+            convertView as PageView
         }
 
         val pageSize = pageSizes[position]
 
         if (pageSize != null) {
-            // We already know the page size. Set it up
-            // immediately
+            // We already know the page size. Set it up immediately.
             pageView.setPage(position, pageSize)
         } else {
-            // Page size as yet unknown. Blank it for now, and
-            // start a background task to find the size
+            // Page size as yet unknown. Blank it for now, and compute in a background.
             pageView.blank(position)
-
-            val sizingTask: AsyncTask<Void?, Void?, PointF?> = object : AsyncTask<Void?, Void?, PointF?>() {
-
-                    protected override fun doInBackground(vararg params: Void?): PointF? {
-                        return try {
-                            muPDFCore.getPageSize(position)
-                        } catch (e: RuntimeException) {
-                            null
-                        }
-                    }
-
-                    override fun onPostExecute(result: PointF?) {
-                        super.onPostExecute(result)
-                        // We now know the page size
-                        pageSizes.put(position, result)
-                        // Check that this view hasn't been reused for
-                        // another page since we started
-                        if (pageView.page == position) {
-                            pageView.setPage(position, result)
-                        }
-                    }
-                }
-
-            sizingTask.execute(null as Void?)
-
+            sizePageView(position, pageView)
         }
 
         return pageView
 
+    }
+
+    private fun sizePageView(position: Int, pageView: PageView) {
+        CoroutineScope(Dispatchers.Main).launch {
+
+            val result = withContext(Dispatchers.IO) {
+                return@withContext try {
+                    muPDFCore.getPageSize(position)
+                } catch (e: RuntimeException) {
+                    null
+                }
+            }
+
+            pageSizes.put(position, result)
+
+            if (pageView.page == position) {
+                pageView.setPage(position, result)
+            }
+
+        }
     }
 
 }
